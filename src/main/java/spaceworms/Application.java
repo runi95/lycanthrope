748f4f8b287a8d7e471c66bfed4ac4cd82863ccc
runi95 <Runar.Serigstad@student.uib.no>
2018-10-19 16:59:50 +0200

    Moved Application.java and IndexController.java to a new root package

--- src/main/java/spaceworms/Application.java
+++ src/main/java/spaceworms/Application.java
@@ -1,3 +1,5 @@
+package spaceworms;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/spaceworms/controllers/IndexController.java
+++ src/main/java/spaceworms/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package controllers;
+package spaceworms.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
