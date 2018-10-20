f2a289371d9c8bee3d4764f3392429801c574a48
runi95 <Runar.Serigstad@student.uib.no>
2018-10-20 02:29:50 +0200

    Added the possibility to join lobbies

--- src/main/java/spaceworms/controllers/BoardController.java
+++ src/main/java/spaceworms/controllers/BoardController.java
@@ -1,13 +1,34 @@
 package spaceworms.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import spaceworms.models.Board;
+import spaceworms.services.APIService;
+
+import java.util.Optional;
 
 @Controller
 public class BoardController {
 
+    @Autowired
+    APIService apiService;
+
     @GetMapping("/boards")
     public String getBoards() {
         return "boards";
+    }
+
+    @GetMapping("/boards/{boardId}")
+    public String getBoard(@PathVariable("boardId") Integer boardId, Model model) {
+        Optional<Board> optionalBoard = apiService.getBoard(boardId);
+
+        if (optionalBoard.isPresent()) {
+            model.addAttribute("board", optionalBoard.get());
+        }
+
+        return "boardLobby";
     }
 }
