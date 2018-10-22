8982bb2727daa9a37238d48ba9e3df84dc476d81
runi95 <Runar.Serigstad@student.uib.no>
2018-10-22 04:20:50 +0200

    Changed print to log

--- src/main/java/spaceworms/models/JSONWrapperModel.java
+++ src/main/java/spaceworms/models/JSONWrapperModel.java
@@ -2,6 +2,8 @@ package spaceworms.models;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,7 +12,8 @@ import java.util.Optional;
 
 public class JSONWrapperModel<E> {
 
-    // TODO: Figure out why Jackson didn't like it when I created the typeReference inside this method
+    Logger logger = LoggerFactory.getLogger(JSONWrapperModel.class);
+
     public Optional<E> readJSONValueFromStreamAndHandleErrors(InputStream inputStream, TypeReference<E> typeReference, int responseCode) {
         if (responseCode == HttpURLConnection.HTTP_OK) {
             return this.readJSONValueFromStream(inputStream, typeReference);
@@ -20,7 +23,7 @@ public class JSONWrapperModel<E> {
 
             // For now we simply print errors we get from the API
             if (optionalError.isPresent()) {
-                printError(optionalError.get());
+                logError(optionalError.get());
             }
 
             return Optional.empty();
@@ -43,8 +46,7 @@ public class JSONWrapperModel<E> {
         return Optional.of(parsedJSONValue);
     }
 
-    // TODO: Change this method from print to log
-    private static void printError(Error error) {
-        System.err.printf("%s\t Error(%d): %s\n", error.getLevel(), error.getCode(), error.getMessage());
+    private void logError(Error error) {
+        logger.error(error.getMessage());
     }
 }
